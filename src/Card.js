78dfee868f1fe491c4ab3119c0a559609971dfe6
Docstone <darochamtth@gmail.com>
2021-04-25 17:32:48 +0200

    added searchbox and cardlist

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
+
+const Card = ({ name, username, email, id }) => {
 	return (
 	<div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-		<img src={`https://robohash.org/${id}?200x200`} alt='robofriend'/>
+		<img src={`https://robohash.org/${id}?`} alt='robofriend'/>
 		<div>
 			<h2>{name}</h2>
+			<h3>{username}</h3>
 			<p>{email}</p>
 		</div>
 	</div>
--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
-import Card from './Card';
-import { robots } from './robots';
+import App from './App';
 
 ReactDOM.render(
-				<div>
-					<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-					<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-					<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-				</div>,
+				<App />,
   document.getElementById('root')
 );
 
@@ -19,3 +14,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
